6f9b64e41a83c2d7477761c4717700d0c87a5c4d
Hashim Ali Zaidi <62440904+hashimali179@users.noreply.github.com>
2023-07-17 10:02:45 +0530

    organize proj

--- portfolio/src/components/Project/Project.jsx
+++ portfolio/src/components/Project/Project.jsx
@@ -21,6 +21,42 @@ const Project = () => {
 						 isLink={true}
 						 />
 					</div>
+					
+					<div className="col-lg-4 col-md-4 col-sm-12 col-12 my-5 my-lg-0 my-md-0">
+						<Card
+							 img={ToDoList}
+							 title="To Do List App"
+							 text="To Do List App made with React and django rest framework."
+							 link="https://kamar-todo.netlify.app/"
+						 	 isLink={true}
+
+							 />
+					</div>
+					
+					<div className="col-lg-4 col-md-4 col-sm-12 col-12">
+						<Card
+						 img={JsChallenge}
+						 title="30 Days Of JavaScript"
+						 text="A 30 Days Of JavaScript challenge to learn JavaScript by making 30 different projects."
+						 link="https://simplykamar.me/30DaysOfJavaScript/30DaysOfJs/home.html"
+						 isLink={true}
+
+						 />
+					</div>
+				</div>	
+
+				<div className="row mt-5">
+					
+					<div className="col-lg-4 col-md-4 col-sm-12 col-12 my-5 my-lg-0 my-md-0">
+						<Card
+							 img={FoodOrdering}
+							 title="Food Ordering System"
+							 text="The Online Food ordering system is a process in which one can order various foods and beverages from some local restaurant and hotels through the use of internet, just by sitting at home or any place. And the order is delivered to the told location."
+							 link="https://restro.pythonanywhere.com/foodie/"
+						 	 isLink={true}
+
+							 />
+					</div>
 					<div className="col-lg-4 col-md-4 col-sm-12 col-12 my-5 my-lg-0 my-md-0">
 						<Card
 						 img={MailSystem}
@@ -40,39 +76,6 @@ const Project = () => {
 						 isLink={true}
 
 						 />
-					</div>
-				</div>	
-
-				<div className="row mt-5">
-					<div className="col-lg-4 col-md-4 col-sm-12 col-12">
-						<Card
-						 img={JsChallenge}
-						 title="30 Days Of JavaScript"
-						 text="A 30 Days Of JavaScript challenge to learn JavaScript by making 30 different projects."
-						 link="https://simplykamar.me/30DaysOfJavaScript/30DaysOfJs/home.html"
-						 isLink={true}
-
-						 />
-					</div>
-					<div className="col-lg-4 col-md-4 col-sm-12 col-12 my-5 my-lg-0 my-md-0">
-						<Card
-							 img={FoodOrdering}
-							 title="Food Ordering System"
-							 text="The Online Food ordering system is a process in which one can order various foods and beverages from some local restaurant and hotels through the use of internet, just by sitting at home or any place. And the order is delivered to the told location."
-							 link="https://restro.pythonanywhere.com/foodie/"
-						 	 isLink={true}
-
-							 />
-					</div>
-					<div className="col-lg-4 col-md-4 col-sm-12 col-12 my-5 my-lg-0 my-md-0">
-						<Card
-							 img={ToDoList}
-							 title="To Do List App"
-							 text="To Do List App made with React and django rest framework."
-							 link="https://kamar-todo.netlify.app/"
-						 	 isLink={true}
-
-							 />
 					</div>
 					
 				</div>			
